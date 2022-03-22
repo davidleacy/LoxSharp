3d451d49ff672954188925930f27170feefac9cd
David Leacy <davidleacy@users.noreply.github.com>
2022-03-22 17:26:31 +0000

    Chap 5: Create AST expression generator and pretty printer

---
+++ LoxSharp/Extensions/StringExtentions.cs
@@ -0,0 +1,13 @@
+﻿namespace LoxSharp.Extensions;
+
+internal static class StringExtentions
+{
+    /// <summary>
+    /// Retrieves a substring from this instance. The substring starts at a specified character position <paramref name="startIndex"/> and ends at the <paramref name="endIndex"/> exclusive.
+    /// </summary>
+    /// <param name="mainString">The string instance to operate on.</param>
+    /// <param name="startIndex">The start index inclusive.</param>
+    /// <param name="endIndex">The end index exclusive to mirror the Java implementation used in the Crafting Interpreters book.</param>
+    /// <returns>The substring.</returns>
+    public static string SubstringByIndex(this string mainString, int startIndex, int endIndex) => mainString.Substring(startIndex, endIndex - startIndex);
+}
--- LoxSharp/StringExtentions.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace LoxSharp;
-
-internal static class StringExtentions
-{
-    public static string SubstringByIndex(this string mainString, int startIndex, int endIndex) => mainString.Substring(startIndex, endIndex - startIndex);
-}
